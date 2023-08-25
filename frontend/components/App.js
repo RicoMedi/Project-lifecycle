9ec451e5139b692b6c6be2c124d4bc272cd59012
Madcodes831 <r.medina2119@gmail.com>
2023-08-24 17:47:29 -0700

    added more handlers

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -10,41 +10,32 @@ export default class App extends React.Component {
         enterName:'',
         }
   
- 
+//initial axios get
 fetchAllTodos= ()=>{
       axios.get(URL)
       .then(res =>{
         this.setState({...this.state, todos: res.data.data})
-              
+         
       })
-      .catch(err => {
-       this.setState({...this.state, error: `Error: ${err.response.data.message}`})
-        
-      })
+      .catch(this.setAxiosResponseError)
     }
+
+    //component is mounted rendering 
  componentDidMount(){
+  //initializing fetchalltodos so that it displays
       this.fetchAllTodos()
   
       
     }
-//another Helper to post a new to do
-    onInputChange  = event => {
+
+
+    //another handlers to post a new to do
+    //this one controls my input box values
+onInputChange  = event => {
         const { value } = event.target
             this.setState({...this.state, enterName: value})
   
             }
-
-//axios post takes in the URL where we posting and what we submitting
- postNewTodo = () => {
-  axios.post(URL, {name: this.state.enterName})
-  .then(res =>{
-    this.fetchAllTodos()
-    this.setState({...this.state, enterName: ''})
-  })
-  .catch(err=>{
-    this.setState({...this.state, error: err.response.data.message})
-  })
- }
 
 //this is my submitHandler
 //prevents reload
@@ -53,6 +44,36 @@ fetchAllTodos= ()=>{
   event.preventDefault()
   this.postNewTodo()
  }
+
+//axios post takes in the URL where we posting and what we submitting 
+  postNewTodo = () => {
+    axios.post(URL, {name: this.state.enterName})
+    .then(res =>{
+       this.fetchAllTodos()
+       this.clearInputBox()
+      })
+      .catch( this.setAxiosResponseError)
+    
+    }
+
+
+//this clear the enter task input box after posting 
+ clearInputBox =() =>{
+  this.setState({...this.state, enterName: ''})
+ }
+ 
+ //this handler displays Errors, made it so that i have dry code instead of it being repetitive, used in the .catch
+ setAxiosResponseError =(err)=>{
+  console.log(err)
+  this.setState({...this.state, error:`Error:${err.response.data.message}` })
+ }
+
+ 
+ 
+
+
+
+
 
 
   render() {
